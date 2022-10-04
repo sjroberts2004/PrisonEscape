cce8a2e4f0f11afed704fabcddbe07f8e6f2ba5d
csteven14 <66684782+csteven14@users.noreply.github.com>
2022-10-04 18:03:11 -0500

    Updated combat screen
    new overlay, positions of allies/enemies can be set with pos variable (0-3), health bar moves with sprite

--- Assets/Scripts/Combat_scripts/statblock.cs
+++ Assets/Scripts/Combat_scripts/statblock.cs
@@ -4,29 +4,37 @@ using UnityEngine;
 
 public class statblock : MonoBehaviour
 {
+
   public int HP;
   public int ATK;
   public int DEF;
   public int ACC;
   public bool enemy;
-  
+  public int pos;
+  private int yline = 120;
+  private int x;
+  private Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
-
+      position = new Vector3(0,0.5F,0);
     }
     void Update(){
-
+      if(enemy){
+        x = 520+(60*pos);
+        position.x = 1.5F+(pos);
+      }
+      else{
+        x = 345-(60*pos);
+        position.x = -1.5F-(pos);
+      }
+      transform.position = position;
     }
     // Update is called once per frame
     void OnGUI()
     {
-      if(!enemy){
-        GUI.Label(new Rect(225,200,475,330), HP.ToString());
-      }
-      else{
-        GUI.Label(new Rect(325,200,600,330), HP.ToString());
-      }
+      GUI.Label(new Rect(x,yline,x,yline), HP.ToString());
+
 
     }
 }
