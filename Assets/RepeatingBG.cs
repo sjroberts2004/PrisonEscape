d2fdd89e49038facc5c9375ad2811d6eb1df7b55
userword <tonifaseemo@gmail.com>
2022-10-03 21:24:03 -0500

    Scrolling Background

---
+++ Assets/RepeatingBG.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBG : MonoBehaviour
+{
+    Rigidbody2D rb;
+    BoxCollider2D boxCollider;
+    float width;
+    public GameObject player;
+
+    void Start()
+    {
+
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        width = boxCollider.size.x;
+
+    }
+
+    void Update()
+    {
+        if (player.GetComponent<Rigidbody2D>().position.x >= 
+            rb.position.x + (width * 0.6)) {
+
+            Vector2 vector = new Vector2(width * 1.5f, 0);
+            transform.position = (Vector2)transform.position + vector;
+        
+        }
+        
+    }
+}
