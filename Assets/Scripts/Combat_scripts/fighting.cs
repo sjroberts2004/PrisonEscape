aa15d03360d6ca1f94ce03c8be16e900e5455473
csteven14 <66684782+csteven14@users.noreply.github.com>
2022-10-06 20:53:26 -0500

    Basic Fight Button Functionality
    Button removes hp from both characters equal to their respective atk stats. When a character falls to 0, the sprite is destroyed.

--- Assets/Scripts/Combat_scripts/fighting.cs
+++ Assets/Scripts/Combat_scripts/fighting.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class fighting : MonoBehaviour
 {
-  public GameObject enemy;
-  public GameObject player;
-  
+  public GameObject enemylead;
+  public GameObject playerlead;
+
+  public void fight(){
+     enemylead.GetComponent<statblock>().HP -= playerlead.GetComponent<statblock>().ATK;
+     if (enemylead.GetComponent<statblock>().HP<=0){
+       Destroy(enemylead);
+     }
+     playerlead.GetComponent<statblock>().HP -= enemylead.GetComponent<statblock>().ATK;
+     if (playerlead.GetComponent<statblock>().HP<=0){
+       Destroy(playerlead);
+     }
+
+  }
 
     // Start is called before the first frame update
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown("space")){
-          enemy.GetComponent<statblock>().HP = enemy.GetComponent<statblock>().HP-5;
+
         }
     }
 }
